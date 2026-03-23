fd5de009ee2911caac2fb70e55b8f1714b9f4ae9
Jayeshv18 <jayeshv@karunya.edu.in>
2026-03-23 23:52:42 +0530

    Implement cd built-in (handles arguments, HOME fallback, and chdir errors)

--- main.c
+++ main.c
@@ -17,9 +17,32 @@ int main() {
         args[i]=strtok(buffer," \t\n"); //Split whenever you see ANY ONE of these characters ie ' ' (space) or '\n' (newline)
         while (args[i]!=NULL) {
             i++;
-            args[i]=strtok(NULL," \t\n"); // Null in strtok means, continue from where you left last time cause otherwise it'll always give the first element and keep stoping there.
+            args[i]=strtok(NULL," \t\n");
+            // Null in strtok means, continue from where you left last time cause otherwise it'll always give the first element and keep stoping there.
         }
         if (args[0] == NULL) continue; //If user presses enter, crash handling
+        if (strcmp(args[0], "cd")==0) {
+
+            if (args[1]!=NULL) {
+                int status=chdir(args[1]); //store result
+                if (status==-1) { //handle error
+                    perror("Change directory failed");
+                }
+            }
+            else {
+                char* home=getenv("HOME"); //getenv is used to get the environmental variables. else case if the user does cd and enter without arguments.
+                if (home == NULL) {
+                    fprintf(stderr,"cd: HOME not set.\n"); // If home doesn't exists or the env isn't set
+                }else {
+                    int res=chdir(home);
+                    if (res == -1) {
+                        perror("Change directory failed");
+                    }
+                }
+            }
+            continue; //skip the current iteration once cd is called cause cd doesn't requires fork() or execvp. It acts on parent process and not on child.
+        }
+        
         pid_t pid = fork(); // pid_t is a specialized data type used to represent Process IDs
         if (pid<0) {
             perror("Process creation failed");
